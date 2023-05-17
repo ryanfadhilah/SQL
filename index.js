0f63e2c3e74638406440cca4405580711149abea
ryanfadhilah <ryan.fadhilah.acc@gmail.com>
2023-05-17 16:25:10 +0700

    first setup

--- index.js
+++ index.js
@@ -6,3 +6,14 @@ const express = require('express')
 const cors = require('cors')
 const mysql = require('mysql')
 
+const app = express()
+const PORT = 3300
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.status(200).send('<h4> Integrated with SQL Expres</h4>')
+})
+
+app.listen(PORT, () => { console.log(`API Running at : ${PORT}`) })
